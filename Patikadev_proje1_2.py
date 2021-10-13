69bf9bb13c5890949013af6862c62feb6ff28983
Mehmet Fatih Sönmez <mfsonmez@gyhmetro.local>
2021-10-13 08:50:09 +0300

    patikadevproje-1

---
+++ Patikadev_proje1_2.py
@@ -0,0 +1,24 @@
+def flatten(A):
+    F=[]
+    for i in A:
+        if type(i) == list:
+            for x in i:
+                if type(x) == list:
+                    for y in x:
+                        if type(y) == list:
+                            for z in y:
+                                F.append(z)
+                        else:
+                            F.append(y)
+                else:
+                    F.append(x)
+        else:
+            F.append(i)
+    return F[::-1]
+
+
+
+A=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+print(flatten(A))
+
+
